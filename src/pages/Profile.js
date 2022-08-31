744557a398da401261ae9509a0eba3879c59b17f
Caiosev <kaiosev9@gmail.com>
2022-08-30 21:13:02 -0300

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../CSS/Profile.css';
 
 function Profile() {
   const { email } = JSON.parse(localStorage.getItem('user'));
@@ -11,33 +12,42 @@ function Profile() {
       <Header title="Profile" />
       <p
         data-testid="profile-email"
+        className="profile-email"
       >
-        { email }
+        Você esta logado como:
+        {' '}
+        {' '}
+        {email}
       </p>
-      <button
-        onClick={ () => history.push('/done-recipes') }
-        data-testid="profile-done-btn"
-        type="button"
-      >
-        Done Recipes
-      </button>
-      <button
-        onClick={ () => history.push('/favorite-recipes') }
-        data-testid="profile-favorite-btn"
-        type="button"
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => {
-          history.push('/');
-          localStorage.clear();
-        } }
-      >
-        Logout
-      </button>
+      <div className="wrapper-buttons">
+        <button
+          onClick={ () => history.push('/favorite-recipes') }
+          data-testid="profile-favorite-btn"
+          type="button"
+          className="btns-profile"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          onClick={ () => history.push('/done-recipes') }
+          data-testid="profile-done-btn"
+          type="button"
+          className="btns-profile"
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            history.push('/');
+            localStorage.clear();
+          } }
+          className="btns-profile"
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
     </div>
   );
